cf28920848d972392d174255b990ac01b4a86e3c
Stephen McLeod <stephen.m.mcleod@gmail.com>
2012-09-16 21:29:16 -0400

    beat level 4

--- prego-beginner/player.rb
+++ prego-beginner/player.rb
@@ -3,11 +3,12 @@ class Player
     if warrior.feel.enemy?
       warrior.attack!
     else
-      if warrior.health < 20
+      if warrior.health < 20 && (@health != nil && warrior.health >= @health)
         warrior.rest!
       else
         warrior.walk!
       end
     end    
+    @health = warrior.health
   end
 end
